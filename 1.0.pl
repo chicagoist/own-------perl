04295d6e8de2a850f754faa631b31965ab6d7e0a
chicagoist <le.legioner@gmail.com>
2021-11-24 22:03:02 +0200

    +7 (999) 999-99-99

---
+++ 1.0.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl -w
+#===============================================================================
+#
+#         FILE:  1.0.pl
+#
+#        USAGE:  ./1.0.pl  
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Valerii Dubkoff
+#      COMPANY:  
+#      VERSION:  1.0
+#      CREATED:  24.11.2021 21:10:24
+#     REVISION:  ---
+#===============================================================================
+
+# https://github.com/chicagoist/Exercises_From_LearningPerl.git
+# https://www.learning-perl.com/
+# https://www.linkedin.com/in/legioneroff/
+
+
+use 5.10.0;
+use strict;
+use open qw( :std :encoding(UTF-8) );
+binmode(STDIN, ':utf8');
+binmode(STDOUT, ':utf8');
+use utf8;
+use warnings;
+use POSIX;
+
+my $input = <>;
+
+#print $input =~ /^(?!\W$)(\+7\s?[(]?[0-9]{3}[)]?\s?[0-9]{3}[-| ]?[0-9]{2}[-| ]?[0-9]{2})$/ ?  "OK\n" : "Not OK\n"; 
+# тут пробовал +79999999999 или +7 999 999 99 99 но не более одного пробела или одного минуса
+# но это не ТЗ в задаче
+
+
+print $input =~ /^(?!\W$)(\+7\s{1}[(]{1}[0-9]{3}[)]{1}\s{1}[0-9]{3}[-]{1}[0-9]{2}[-]{1}[0-9]{2})$/ ? "OK\n":"Not OK\n"; 
+
+
+
+=begin  BlockComment  # BlockCommentNo_1
+
+Напишите программу валидатор Российских номеров в формате +7 (xxx) xxx-xx-xx
+
+Sample Input 1:
+
++7 (999) 999-99-99
+
+Sample Output 1:
+
+OK
+.....................
+Sample Input 2:
+
++79999999999
+
+Sample Output 2:
+
+Not OK
+
+
+=end    BlockComment  # BlockCommentNo_1
+
+=cut
