f9d24cb63bd97c323007da912c7d7e670e478d8a
chicagoist <31587029+chicagoist@users.noreply.github.com>
2021-11-27 12:12:49 +0200

    fix

--- 1.0.pl
+++ 1.0.pl
@@ -25,45 +25,36 @@
 
 use 5.10.0;
 use strict;
-use open qw( :std :encoding(UTF-8) );
+use open qw(:std :encoding(UTF-8));
 binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
 use utf8;
 use warnings;
 use POSIX;
 
-my $input = <>;
 
-#print $input =~ /^(?!\W$)(\+7\s?[(]?[0-9]{3}[)]?\s?[0-9]{3}[-| ]?[0-9]{2}[-| ]?[0-9]{2})$/ ?  "OK\n" : "Not OK\n"; 
+
+=begin   BlockCommentNo_1
+Напишите программу валидатор Российских номеров в формате +7 (xxx) xxx-xx-xx
+Sample Input 1:
++7 (999) 999-99-99
+Sample Output 1:
+OK
+.....................
+Sample Input 2:
++79999999999
+Sample Output 2:
+Not OK
+=end     BlockCommentNo_1
+=cut
+
+
+
+my $input = <>;
+print $input =~ /^(?!\W$)(\+7\s{1}[(]{1}[0-9]{3}[)]{1}\s{1}[0-9]{3}[-]{1}[0-9]{2}[-]{1}[0-9]{2})$/ ? "OK\n" : "Not OK\n";
+
+#print $input =~ /^(?!\W$)(\+7\s?[(]?[0-9]{3}[)]?\s?[0-9]{3}[-| ]?[0-9]{2}[-| ]?[0-9]{2})$/ ?  "OK\n" : "Not OK\n";
 # тут пробовал +79999999999 или +7 999 999 99 99 но не более одного пробела или одного минуса
 # но это не ТЗ в задаче
 
 
-print $input =~ /^(?!\W$)(\+7\s{1}[(]{1}[0-9]{3}[)]{1}\s{1}[0-9]{3}[-]{1}[0-9]{2}[-]{1}[0-9]{2})$/ ? "OK\n":"Not OK\n"; 
-
-
-
-=begin  BlockComment  # BlockCommentNo_1
-
-Напишите программу валидатор Российских номеров в формате +7 (xxx) xxx-xx-xx
-
-Sample Input 1:
-
-+7 (999) 999-99-99
-
-Sample Output 1:
-
-OK
-.....................
-Sample Input 2:
-
-+79999999999
-
-Sample Output 2:
-
-Not OK
-
-
-=end    BlockComment  # BlockCommentNo_1
-
-=cut
