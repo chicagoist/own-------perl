b51a8a6087f8ee6bdb2b3e67c04bac933ecfece4
chicagoist <le.legioner@gmail.com>
2021-12-07 16:04:00 +0200

    exercies from chapter 9

---
+++ change_fred03dat.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use 5.10.0;
+use strict;
+use warnings FATAL => 'all';
+use diagnostics;
+use open qw(:std :encoding(UTF-8));
+use Encode qw(decode_utf8);
+#BEGIN{@ARGV = map decode_utf8($_, 1),@ARGV;}
+binmode(STDIN, ':utf8');
+binmode(STDOUT, ':utf8');
+use utf8::all 'GLOBAL';
+use Encode::Locale;
+#use CGI;
+#use POSIX;
+#use utf8;
+
+
+# Обновление нескольких файлов
+# В следующем примере используются сотни файлов, имеющих похоQ жий формат.
+# Один из них, fred03.dat, содержит строки следующего вида:
+#
+#    Program name: granite
+#    Author: Gilbert Bates
+#    Company: RockSoft
+#    Department: R&D
+#    Phone: +1 503 555-0095
+#    Date: Tues March 9, 2004
+#    Version: 2.1
+#    Size: 21k
+#    Status: Final beta
+
+# Требуется обновить файл и включить в него другую информацию. ПоQсле обновления файл должен выглядеть примерно так:
+#
+#    Program name: granite
+#    Author: Randal L. Schwartz
+#    Company: RockSoft
+#    Department: R&D
+#    Date: December 15, 2021 6:38 pm
+#    Version: 2.1
+#    Size: 21k Status:
+#    Final beta
+#
+chomp(@_);
+chomp(my $date = `date`);
+$^I = ".bak";
+while (<>) {
+    s/\bAuthor:.*/Author: Randal L. Schwartz/;
+    s/\bPhone:.*\n//;
+    s/\bDate:.*/Date: $date/;
+    print;
+}
+
+
+
+
+
